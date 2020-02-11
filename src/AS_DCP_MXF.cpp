9ae11d9b59d5aac74fd7849d4d0acc7f3f321f9e
Wolfgang Ruppel <wolfgangruppel@Wolfgangs-MacBook-Pro.local>
2020-02-11 16:38:56 +0100

    Bug in ProRes 422 detection fixed

--- src/AS_DCP_MXF.cpp
+++ src/AS_DCP_MXF.cpp
@@ -243,26 +243,28 @@ ASDCP::EssenceType(const std::string& filename, EssenceType_t& type)
 	  }
 	  if (type == ESS_UNKNOWN)
 	  {
-	    MXF::RGBAEssenceDescriptor *rgba_descriptor = 0;
-	    char buf[64];
+		  if (ASDCP_SUCCESS(TestHeader.GetMDObjectByType(OBJ_TYPE_ARGS(RGBAEssenceDescriptor))) ){
+			MXF::RGBAEssenceDescriptor *rgba_descriptor = 0;
+			char buf[64];
 
-	    if ASDCP_SUCCESS(TestHeader.GetMDObjectByType(m_Dict->ul(MDD_RGBAEssenceDescriptor), reinterpret_cast<MXF::InterchangeObject**>(&rgba_descriptor)))
-	    {
-	        if (rgba_descriptor->EssenceContainer == m_Dict->ul(MDD_MXFGCFrameWrappedEssenceContainerProResPicture))
-	            type = ESS_AS02_ProRes;
-	    }
-	  }
+			if ASDCP_SUCCESS(TestHeader.GetMDObjectByType(m_Dict->ul(MDD_RGBAEssenceDescriptor), reinterpret_cast<MXF::InterchangeObject**>(&rgba_descriptor)))
+			{
+				if (rgba_descriptor->EssenceContainer == m_Dict->ul(MDD_MXFGCFrameWrappedEssenceContainerProResPicture))
+					type = ESS_AS02_ProRes;
+			}
+		  }
 
-	  else if (ASDCP_SUCCESS(TestHeader.GetMDObjectByType(OBJ_TYPE_ARGS(CDCIEssenceDescriptor))) )
-	  {
-	    MXF::CDCIEssenceDescriptor *cdci_descriptor = 0;
-	    char buf[64];
+		  else if (ASDCP_SUCCESS(TestHeader.GetMDObjectByType(OBJ_TYPE_ARGS(CDCIEssenceDescriptor))) )
+		  {
+			MXF::CDCIEssenceDescriptor *cdci_descriptor = 0;
+			char buf[64];
 
-	    if ASDCP_SUCCESS(TestHeader.GetMDObjectByType(m_Dict->ul(MDD_CDCIEssenceDescriptor), reinterpret_cast<MXF::InterchangeObject**>(&cdci_descriptor)))
-	    {
-	        if (cdci_descriptor->EssenceContainer == m_Dict->ul(MDD_MXFGCFrameWrappedEssenceContainerProResPicture))
-	            type = ESS_AS02_ProRes;
-	    }
+			if ASDCP_SUCCESS(TestHeader.GetMDObjectByType(m_Dict->ul(MDD_CDCIEssenceDescriptor), reinterpret_cast<MXF::InterchangeObject**>(&cdci_descriptor)))
+			{
+				if (cdci_descriptor->EssenceContainer == m_Dict->ul(MDD_MXFGCFrameWrappedEssenceContainerProResPicture))
+					type = ESS_AS02_ProRes;
+			}
+		  }
 	  }
 	  if (type == ESS_UNKNOWN)
 	  {
